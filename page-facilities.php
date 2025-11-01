a3eb15ec7302141cda8564b59a7edc99da25a853
yumauda <firststep-yumauda@gmail.com>
2025-11-01 13:22:15 +0900

    facilitiesページ

---
+++ page-facilities.php
@@ -0,0 +1,265 @@
+<div class="p-top">
+
+  <?php get_header(); ?>
+  <main>
+    <section class="p-page-mv">
+      <div class="p-page-mv__inner">
+        <div class="p-page-mv__content">
+          <figure class="p-page-mv__img">
+            <img src="<?php echo get_template_directory_uri() ?>/images/common/mv_facilities.jpg" alt="館内案内" width="1860" height="670">
+          </figure>
+          <div class="p-page-mv__detail">
+            <h2 class="p-page-mv__title">
+              <span class="p-page-mv__title-ja">館内のご案内</span>
+              <span class="p-page-mv__title-en">Facilities</span>
+            </h2>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="p-guide">
+      <div class="l-inner">
+        <div class="p-guide__content">
+          <div class="p-guide__title">
+            <h2 class="c-main-title">
+              <span class="c-main-title__ja">フロアガイド</span>
+              <span class="c-main-title__en">Floor guide</span>
+            </h2>
+          </div>
+
+          <div class="p-guide__main">
+            <div class="p-guide__left">
+              <div class="p-guide__box">
+                <div class="p-guide__floor-item">
+                  <span class="p-guide__floor-number">4-6F</span>
+                  <div class="p-guide__floor-facilities">
+                    <p class="p-guide__facility-name">客室</p>
+                  </div>
+                </div>
+
+                <div class="p-guide__floor-item">
+                  <span class="p-guide__floor-number">3F</span>
+                  <div class="p-guide__floor-facilities">
+                    <p class="p-guide__facility-name">結婚式場・写真室・美容室・衣裳室</p>
+                    <p class="p-guide__facility-name">更衣室・さつきの間・あやめの間</p>
+                  </div>
+                </div>
+
+                <div class="p-guide__floor-item">
+                  <span class="p-guide__floor-number">2F</span>
+                  <div class="p-guide__floor-facilities">
+                    <p class="p-guide__facility-name">大宴会場「鶴の間」・中宴会場「桜の間」</p>
+                    <p class="p-guide__facility-name">「高瀬の間」・「那岐の間」「弥生の間」</p>
+                    <p class="p-guide__facility-name">和式宴会場「衆楽の間」</p>
+                  </div>
+                </div>
+
+                <div class="p-guide__floor-item">
+                  <span class="p-guide__floor-number">1F</span>
+                  <div class="p-guide__floor-facilities">
+                    <p class="p-guide__facility-name">フロント・クローク・レストラン「和」</p>
+                    <p class="p-guide__facility-name">売店・ロビー・CHAPEL「ベネディクタス」</p>
+                  </div>
+                </div>
+
+                <div class="p-guide__floor-item">
+                  <span class="p-guide__floor-number">B1F</span>
+                  <div class="p-guide__floor-facilities">
+                    <p class="p-guide__facility-name">駐車場</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+            <div class="p-guide__right">
+              <ul class="p-guide__list">
+                <li class="p-guide__list-item">
+                  <span class="p-guide__list-floor">4-6F</span>
+                  <div class="p-guide__list-box">
+                    <a href="#" class="p-guide__list-link">
+                      <span class="p-guide__list-name">客室</span>
+                      <div class="p-guide__list-icon">
+                        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/news_arrow.svg" alt="" width="15" height="8">
+                      </div>
+                    </a>
+                  </div>
+                </li>
+
+                <li class="p-guide__list-item">
+                  <span class="p-guide__list-floor">3F</span>
+                  <div class="p-guide__list-box">
+                    <a href="#" class="p-guide__list-link">
+                      <span class="p-guide__list-name">結婚式場</span>
+                      <div class="p-guide__list-icon">
+                        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/news_arrow.svg" alt="" width="15" height="8">
+                      </div>
+                    </a>
+                    <a href="#" class="p-guide__list-link">
+                      <span class="p-guide__list-name">さつきの間</span>
+                      <div class="p-guide__list-icon">
+                        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/news_arrow.svg" alt="" width="15" height="8">
+                      </div>
+                    </a>
+                    <a href="#" class="p-guide__list-link">
+                      <span class="p-guide__list-name">あやめの間</span>
+                      <div class="p-guide__list-icon">
+                        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/news_arrow.svg" alt="" width="40" height="40">
+                      </div>
+                    </a>
+                  </div>
+                </li>
+
+                <li class="p-guide__list-item">
+                  <span class="p-guide__list-floor">2F</span>
+                  <div class="p-guide__list-box">
+                    <a href="#" class="p-guide__list-link">
+                      <span class="p-guide__list-name">鶴の間</span>
+                      <div class="p-guide__list-icon">
+                        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/news_arrow.svg" alt="" width="40" height="40">
+                      </div>
+                    </a>
+                    <a href="#" class="p-guide__list-link">
+                      <span class="p-guide__list-name">桜の間</span>
+                      <div class="p-guide__list-icon">
+                        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/news_arrow.svg" alt="" width="40" height="40">
+                      </div>
+                    </a>
+                    <a href="#" class="p-guide__list-link">
+                      <span class="p-guide__list-name">高瀬の間</span>
+                      <div class="p-guide__list-icon">
+                        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/news_arrow.svg" alt="" width="40" height="40">
+                      </div>
+                    </a>
+                    <a href="#" class="p-guide__list-link">
+                      <span class="p-guide__list-name">那岐の間</span>
+                      <div class="p-guide__list-icon">
+                        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/news_arrow.svg" alt="" width="40" height="40">
+                      </div>
+                    </a>
+                    <a href="#" class="p-guide__list-link">
+                      <span class="p-guide__list-name">弥生の間</span>
+                      <div class="p-guide__list-icon">
+                        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/news_arrow.svg" alt="" width="40" height="40">
+                      </div>
+                    </a>
+                    <a href="#" class="p-guide__list-link">
+                      <span class="p-guide__list-name">衆楽の間</span>
+                      <div class="p-guide__list-icon">
+                        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/news_arrow.svg" alt="" width="40" height="40">
+                      </div>
+                    </a>
+                  </div>
+                </li>
+
+                <li class="p-guide__list-item">
+                  <span class="p-guide__list-floor">1F</span>
+                  <div class="p-guide__list-box">
+                    <a href="#" class="p-guide__list-link">
+                      <span class="p-guide__list-name">レストラン「和」</span>
+                      <div class="p-guide__list-icon">
+                        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/news_arrow.svg" alt="" width="40" height="40">
+                      </div>
+                    </a>
+                    <a href="#" class="p-guide__list-link">
+                      <span class="p-guide__list-name">チャペル「ベネディクタス」</span>
+                      <div class="p-guide__list-icon">
+                        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/news_arrow.svg" alt="" width="40" height="40">
+                      </div>
+                    </a>
+                  </div>
+
+                </li>
+                <li class="p-guide__list-item">
+                  <span class="p-guide__list-floor">B1F</span>
+                  <div class="p-guide__list-box">
+                    <a href="#" class="p-guide__list-link">
+                      <span class="p-guide__list-name">駐車場</span>
+                      <div class="p-guide__list-icon">
+                        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/news_arrow.svg" alt="" width="40" height="40">
+                      </div>
+                    </a>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="p-facilities">
+      <div class="l-inner">
+        <div class="p-facilities__content">
+          <div class="p-facilities__title">
+            <h3 class="c-main-title">
+              <span class="c-main-title__ja">館内設備</span>
+              <span class="c-main-title__en">Facilities</span>
+            </h3>
+          </div>
+
+          <div class="p-facilities__list">
+            <div class="p-facilities__item">
+              <figure class="p-facilities__img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/facilities_1.jpg" alt="共用サービス" width="360" height="240">
+              </figure>
+              <div class="p-facilities__detail">
+                <h4 class="p-facilities__item-title">共用サービス</h4>
+                <p class="p-facilities__item-text">4Fにはソフトドリンクの自動販売機、ご自由にご利用いただける電子レンジ・製氷機がございます。</p>
+              </div>
+            </div>
+
+            <div class="p-facilities__item">
+              <figure class="p-facilities__img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/facilities_2.jpg" alt="喫煙室" width="360" height="240">
+              </figure>
+              <div class="p-facilities__detail">
+                <h4 class="p-facilities__item-title">喫煙室</h4>
+                <p class="p-facilities__item-text">当ホテルは全館禁煙ですが、4階に専用の喫煙ブースをご用意しております。愛煙家のお客様にも安心してご利用いただける環境を整えております。</p>
+              </div>
+            </div>
+
+            <div class="p-facilities__item">
+              <figure class="p-facilities__img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/facilities_3.jpg" alt="授乳室" width="360" height="240">
+              </figure>
+              <div class="p-facilities__detail">
+                <h4 class="p-facilities__item-title">授乳室</h4>
+                <p class="p-facilities__item-text">小さなお子さまをお連れのお客さまのために、授乳室を完備。個室タイプで気兼ねなくご利用いただけると好評です。</p>
+              </div>
+            </div>
+            <div class="p-facilities__item">
+              <figure class="p-facilities__img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/facilities_4.jpg" alt="パウダールーム" width="360" height="240">
+              </figure>
+              <div class="p-facilities__detail">
+                <h4 class="p-facilities__item-title">パウダールーム</h4>
+                <p class="p-facilities__item-text">大きな鏡をそなえ、快適にご利用いただけるパウダールーム。女性のお客様にうれしい、おもてなしの心づかいです。</p>
+              </div>
+            </div>
+            <div class="p-facilities__item">
+              <figure class="p-facilities__img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/facilities_5.jpg" alt="バリアフリー" width="360" height="240">
+              </figure>
+              <div class="p-facilities__detail">
+                <h4 class="p-facilities__item-title">バリアフリー</h4>
+                <p class="p-facilities__item-text">各会場とも段差をなくし、バリアフリー対策は万全。車イス対応も可能なため、どなたさまも安心してご来館いただけます。</p>
+              </div>
+            </div>
+            <div class="p-facilities__item">
+              <figure class="p-facilities__img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/facilities_6.jpg" alt="売店" width="360" height="240">
+              </figure>
+              <div class="p-facilities__detail">
+                <h4 class="p-facilities__item-title">売店</h4>
+                <p class="p-facilities__item-text">城下町津山の名菓を多数取り揃えています。<br>［営業時間］7:00〜19:00</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <?php get_template_part('includes/submit'); ?>
+  </main>
+  <?php get_footer() ?>
+</div>

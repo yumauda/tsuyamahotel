75b9ac5d2051ad3effcbc3d79c072bbdc1562350
yumauda <firststep-yumauda@gmail.com>
2025-11-01 14:46:34 +0900

    アクセスページ

---
+++ page-access.php
@@ -0,0 +1,161 @@
+<div class="p-top">
+
+  <?php get_header(); ?>
+  <main>
+    <section class="p-page-mv">
+      <div class="p-page-mv__inner">
+        <div class="p-page-mv__content">
+          <figure class="p-page-mv__img">
+            <img src="<?php echo get_template_directory_uri() ?>/images/common/mv_access.jpg" alt="交通のご案内" width="1860" height="670">
+          </figure>
+          <div class="p-page-mv__detail">
+            <h2 class="p-page-mv__title">
+              <span class="p-page-mv__title-ja">交通のご案内</span>
+              <span class="p-page-mv__title-en">Access</span>
+            </h2>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="p-location">
+      <div class="l-inner">
+        <div class="p-location__content">
+          <div class="p-location__title-wrapper">
+            <h3 class="p-location__title">津山の歴史と自然を感じるロケーション</h3>
+          </div>
+          <div class="p-location__text-wrapper">
+            <p class="p-location__text">古き良き歴史と豊かな自然に包まれた津山の街に位置しながら、<br>
+              鉄道やお車で快適にお越しいただける利便性を兼ね備えた最適な立地。<br>
+              お客様の大切なひとときが、ここから始まるように。<br>
+              私たちは上質な旅の玄関口として、皆さまを心よりお迎えいたします。</p>
+          </div>
+          <div class="p-location__map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3265.733998177537!2d134.01718307576033!3d35.063388972793064!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3554314aaa62567f%3A0x2b57a129742e5fbf!2z5rSl5bGx6ba05bGx44Ob44OG44Or!5e0!3m2!1sja!2sjp!4v1761973738430!5m2!1sja!2sjp" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+          <div class="p-location__hotel">
+            <figure class="p-location__img">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/hotel_img.jpg" alt="津山鶴山ホテル" width="600" height="350">
+            </figure>
+            <div class="p-location__hotel-detail">
+              <h4 class="p-location__hotel-title">津山鶴山ホテル</h4>
+              <dl class="p-location__dl">
+                <div class="p-location__row">
+                  <dt class="p-location__dt">所在地</dt>
+                  <dd class="p-location__dd">〒708-0832 岡山県津山市東新町114-4</dd>
+                </div>
+                <div class="p-location__row">
+                  <dt class="p-location__dt">TEL</dt>
+                  <dd class="p-location__dd"><a href="tel:0868252121">0868-25-2121（代）</a></dd>
+                </div>
+                <div class="p-location__row">
+                  <dt class="p-location__dt">FAX</dt>
+                  <dd class="p-location__dd">0868-22-1415</dd>
+                </div>
+              </dl>
+            </div>
+          </div>
+          <dl class="p-location__trafic">
+            <div class="p-location__trafic-row">
+              <dt class="p-location__trafic-dt">
+                お車をご利用の方
+              </dt>
+              <dd class="p-location__trafic-dd">
+                <p class="p-location__trafic-text">
+                  中国自動車道 津山インターから10分
+                </p>
+                <p class="p-location__trafic-text">
+                  ①津山インターチェンジを出て信号を左折
+                </p>
+                <p class="p-location__trafic-text">
+                  ②道なりに直進
+                </p>
+                <p class="p-location__trafic-text">
+                  ③右手にホテル入口が見えてまいります
+                </p>
+              </dd>
+            </div>
+            <div class="p-location__trafic-row">
+              <dt class="p-location__trafic-dt">
+                バスをご利用の方
+              </dt>
+              <dd class="p-location__trafic-dd">
+                <p class="p-location__trafic-text">
+                  《中鉄バス》<br>
+                  ①行方・馬桑線（津山駅～行方～馬桑）<br>
+                  津山駅から約6分 → 天神橋バス停下車 徒歩1分（運賃160円）<br>
+                  ②勝間田駅前線（津山駅～中央病院～イオン津山～勝間田駅前）<br>
+                  津山駅から約6分 → 天神橋バス停下車 徒歩1分（運賃160円）<br>
+                  《ごんごバス》 東循環線（南回り・左回り）<br>
+                  ①津山駅から約13分 → 古林田 または 東新町 下車 徒歩2分（運賃：おとな200円／こども100円）
+                </p>
+              </dd>
+            </div>
+            <div class="p-location__trafic-row">
+              <dt class="p-location__trafic-dt">
+                電車をご利用の方
+              </dt>
+              <dd class="p-location__trafic-dd">
+                <p class="p-location__trafic-text">
+                  JR岡山駅から津山駅まで快速で約1時間10分<br>
+                  ①津山駅から/タクシー10分<br>
+                  ②東津山駅から/タクシー5分
+                </p>
+              </dd>
+            </div>
+
+          </dl>
+        </div>
+    </section>
+    <section class="p-parking">
+      <div class="l-inner">
+        <div class="p-parking__content">
+          <div class="p-parking__title">
+            <h3 class="c-main-title">
+              <span class="c-main-title__ja">駐車場のご案内</span>
+              <span class="c-main-title__en">Parking</span>
+            </h3>
+          </div>
+          <div class="p-parking__row">
+            <figure class="p-parking__img">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/parking_img.jpg" alt="駐車場" width="600" height="350">
+            </figure>
+            <p class="p-parking__text">当ホテルでは、150台収容可能な駐車場（先着順）をご用意しております。
+              地下には屋内駐車場も備えており、雨天時でも快適にご利用いただけます。地下駐車場からはエレベーターで1階へ直結しており、スムーズに館内へお入りいただけます。
+              なお、夜間（22:00～翌6:00）はセキュリティ確保のため閉鎖しております。
+              トラックや大型車のご利用は承っておりませんが、団体宿泊でお越しのバスにつきましては事前にご相談ください。</p>
+          </div>
+          <div class="p-parking__grid">
+            <div class="p-parking__item">
+              <figure class="p-parking__img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/parking_img2.jpg" alt="駐車場" width="600" height="350">
+              </figure>
+              <div class="p-parking__detail">
+                <h4 class="p-parking__subTitle">屋内駐車場</h4>
+                <p class="p-parking__subText">
+                  フロントやお部屋まで直行できる快適な駐車場
+                </p>
+              </div>
+            </div>
+            <div class="p-parking__item">
+              <figure class="p-parking__img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/parking_img3.jpg" alt="駐車場" width="600" height="350">
+              </figure>
+              <div class="p-parking__detail">
+                <h4 class="p-parking__subTitle">二輪向けのスペース</h4>
+                <p class="p-parking__subText">
+                  大切な単車も安心の屋内駐輪場
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+
+    <?php get_template_part('includes/submit'); ?>
+  </main>
+  <?php get_footer() ?>
+</div>
